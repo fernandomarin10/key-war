09d4f0e545f7115a1527b18f41c432f478cad7a7
Fer <marinbcn169@gmail.com>
2022-08-13 23:28:08 +0200

    mvp color size changing on key event

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ let red = 50;
 let blue = 50;
 
 document.addEventListener('DOMContentLoaded', () => {
+    let rightPlayerContainer = document.querySelector(".blue-container");
+    let leftPlayerContainer = document.querySelector(".red-container");
     document.addEventListener('keydown', (event) => {
         if(event.key == "a"){
-            red += 5;
-            blue -=5;
-            console.log({"red": red, "blue": blue})
+            addPointsToLeftPlayer();
         }
         if(event.key == "l"){
-            blue += 5;
-            red -=5;
-            console.log({"red": red, "blue": blue})
+            addPointsToRightPlayer();
         }
+        leftPlayerContainer.style.width = red+"%"
+        rightPlayerContainer.style.width = blue+"%"
       }, false);
 });
+
+
+const addPointsToLeftPlayer = () => {
+    red += 5;
+    blue -= 5;
+    console.log({"red": red, "blue": blue})
+}
+
+const addPointsToRightPlayer = () => {
+    blue += 5;
+    red -=5;
+    console.log({"red": red, "blue": blue})
+}
