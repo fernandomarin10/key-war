e433ebef568f8f4e9bad7e607110835c56fc3957
Fer <marinbcn169@gmail.com>
2022-08-13 23:11:07 +0200

    change buttons for keys

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+let red = 50;
+let blue = 50;
+
 document.addEventListener('DOMContentLoaded', () => {
-    let leftSideHTML = document.querySelector(".left-key");
-    let rightSideHTML = document.querySelector(".right-key");
-    leftSideHTML.addEventListener('click', () => alert("left"));
-    rightSideHTML.addEventListener('click', () => alert("right"));
+    document.addEventListener('keydown', (event) => {
+        if(event.key == "a"){
+            red += 5;
+            blue -=5;
+            console.log({"red": red, "blue": blue})
+        }
+        if(event.key == "l"){
+            blue += 5;
+            red -=5;
+            console.log({"red": red, "blue": blue})
+        }
+      }, false);
 });
